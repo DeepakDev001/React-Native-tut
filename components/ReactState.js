65f51e32569023dd3568c2396d926d793d133a9f
Deepak Kumar <130967081+DeepakDev001@users.noreply.github.com>
2023-09-13 10:26:43 +0530

    update react state

--- components/ReactState.js
+++ components/ReactState.js
@@ -11,12 +11,24 @@ const ReactState = () => {
       <Text style={{ fontSize: 25, marginTop: 20, color: "red" }}>
         State In React Native
       </Text>
-      <Text>{name}</Text>
+      <Text style={{ fontSize: 20 }} >
+        Difference between State and  Variable
+      </Text>
+      <Text >
+        State : - when you Update the value, component will be re-rendering
+      </Text>
+      <Text >
+        State : -state not use the outside the components
+      </Text>
+      <Text >
+        Variable: -when you Update the Variable, component will be not re-renderings
+      </Text>
+      <Text style={{ fontSize: 18, color: "green" }}>{name}</Text>
       <Button
         title='Update Name '
         onPress={() => setName("Yadav")}
       />
-      <Text style={{ fontSize: 24, marginTop: 15 }} >Counter Function</Text>
+      <Text style={{ fontSize: 24, }} >Counter Function</Text>
       <Button
         title='Increment'
         onPress={() => setData(data + 1)}
